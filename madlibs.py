e1417a0c614f3fb104b08075754fbd85e8def043
ewhycode <152671909+ewhycode@users.noreply.github.com>
2024-01-14 00:39:19 +1100

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#string concatenation
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famousPerson = input("Famous person: ")
+
+
+madlib = f"Computer programming is so {adj}! It makes me excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madlib)
